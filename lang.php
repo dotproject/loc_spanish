4b8c689b6269f1eda97b7b52d3ecab285a81ee7b
ajdonnison <ajdonnison>
2004-12-14 06:38:06 +0000

    New language support file for 2.0

---
+++ lang.php
@@ -0,0 +1,9 @@
+<?php
+// Language list for supported locales.
+// This is used in the 2.0 version of dotproject to correctly identify Windows language
+// support.  See the English version for details of its format.
+$dir = basename(dirname(__FILE__));
+
+$LANGUAGES['es_SP'] = array( $dir, 'Spanish', 'Español', 'esp');
+$LANGUAGES['es_MX'] = array( $dir, 'Spanish (MX)', 'Español (MX)', 'esm');
+?>
